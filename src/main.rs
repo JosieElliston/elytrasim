16eaac76a268b23a7570c1b79dcc179950114c5a
HactarCE <6060305+HactarCE@users.noreply.github.com>
2026-04-24 21:05:45 -0400

    Add egui hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+fn main() -> eframe::Result {
+    eframe::run_ui_native(
+        "Eltyra Sim",
+        eframe::NativeOptions::default(),
+        |ui, _frame| {
+            ui.label("Hello, world!");
+        },
+    )
 }
